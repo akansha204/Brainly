e307f41088936c59553dbe74cb4d5d446de6bdc9
Akansha Roy <akansha204>
2025-03-07 01:24:48 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+//import syntax is important as it helpd to implement the type safety.
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+const app = express();
+
+app.post('/api/v1/signup',(req,res)=>{
+
+})
+app.post('/api/v1/signin',(req,res)=>{
+    
+})
+app.post('/api/v1/content',(req,res)=>{
+    
+})
+app.get('/api/v1/content',(req,res)=>{
+    
+})
+app.delete('/api/v1/content',(req,res)=>{
+    
+})
+app.post('/api/v1/brain/share',(req,res)=>{
+    //share the content with the link
+})
+app.get('/api/v1/brain/:shareLink',(req,res)=>{
+    //Fetch another user's shared brain content
+})
